8663163b2522b54bd02ea448ba01d347c60b9128
xiaoluoboding <xiaoluoboding@gmail.com>
2016-02-16 16:35:32 +0800

    Use light green as site style

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,31 +3,29 @@
  */
 
 /* globals jQuery, document */
-(function ($, undefined) {
+(function($, undefined) {
     "use strict";
 
     var $document = $(document);
 
-    $document.ready(function () {
+    $document.ready(function() {
 
         var $postContent = $(".post-content");
         $postContent.fitVids();
 
         $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e) {
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
 
-        $(window).scroll(function(){
+        $(window).scroll(function() {
             var scrollerToTop = $('.backTop');
             var scrollerTOC = $('.widget-toc');
-            var tagCloud = $('.widget-tag-cloud');
-			      document.documentElement.scrollTop+document.body.scrollTop>200?scrollerToTop.fadeIn():scrollerToTop.fadeOut();
-            document.documentElement.scrollTop+document.body.scrollTop>250?scrollerTOC.addClass("widget-toc-fixed"):scrollerTOC.removeClass("widget-toc-fixed");
-            document.documentElement.scrollTop+document.body.scrollTop>250?tagCloud.hide():tagCloud.show();
-		    });
+            document.documentElement.scrollTop + document.body.scrollTop > 200 ? scrollerToTop.fadeIn() : scrollerToTop.fadeOut();
+            document.documentElement.scrollTop + document.body.scrollTop > 250 ? scrollerTOC.addClass("widget-toc-fixed") : scrollerTOC.removeClass("widget-toc-fixed");
+        });
 
         // #backTop Button Event
         $("#backTop").on("click", function() {
@@ -38,16 +36,16 @@
 
     // Arctic Scroll by Paul Adam Davis
     // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+    $.fn.arctic_scroll = function(options) {
 
         var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+                elem: $(this),
+                speed: 500
+            },
 
-        allOptions = $.extend(defaults, options);
+            allOptions = $.extend(defaults, options);
 
-        allOptions.elem.click(function (event) {
+        allOptions.elem.click(function(event) {
             event.preventDefault();
             var $this = $(this),
                 $htmlBody = $('html, body'),
@@ -57,25 +55,35 @@
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top + toMove)
+                }, allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: toMove
+                }, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top)
+                }, allOptions.speed);
             }
         });
 
     };
 })(jQuery);
 
-function scrollToTop(name, speed){
-    if( !speed ) speed = 300
-    if( !name ){
-        $('html,body').animate({scrollTop: 0},speed)
-    }else{
-        if( $(name).length>0 ){
-            $('html,body').animate({scrollTop: $(name).offset().top},speed)
+function scrollToTop(name, speed) {
+    if (!speed) speed = 300
+    if (!name) {
+        $('html,body').animate({
+            scrollTop: 0
+        }, speed)
+    } else {
+        if ($(name).length > 0) {
+            $('html,body').animate({
+                scrollTop: $(name).offset().top
+            }, speed)
         }
     }
 }
